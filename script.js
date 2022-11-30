3b27b6bde1621f95ca84d1fbb9b5875584b30ec7
Bruno Henrique Laviso Adorne <59637443+brunolaviso@users.noreply.github.com>
2022-11-30 19:06:17 -0300

    Finaliza exibicao da lista

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+var pokemons = []
+var listPokemonContentElement = document.querySelector('.list-content')
+
 function fetchPokemons() {
   fetch("https://pokeapi.co/api/v2/pokemon")
     .then((response) => response.json())
-    .then((response) => console.log(response))
+    .then((response) => Promise.all(response.results.map(pokemon => 
+      fetch(pokemon.url)
+        .then((response) => response.json())
+        .then((response) => ({
+          id: response.id,
+          name: response.name,
+          image: response.sprites.other.dream_world.front_default,
+          type: response.types[0].type.name
+        }))
+      )))
+    .then((response) => response.map(pokemon => pokemons.push(pokemon)))
+    .finally(() => listPokemonContentElement.innerHTML = listPokemons())
+}
+
+function listPokemons() {
+  return pokemons.map(pokemon => `
+    <div class="card">
+      <img class="card-image" src=${pokemon.image} alt="">
+      <small class="card-id">${pokemon.id}</small>
+      <strong class="card-name">${pokemon.name}</strong>
+    </div>
+  `).join('')
 }
 
 fetchPokemons()
